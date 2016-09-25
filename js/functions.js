06051b37dd8dc9ae360c9f8b1bdf3a3342010096
WINDOWSDESKTOP\kaden <ksgoodell@crimson.ua.edu>
2016-09-25 13:54:06 -0500

    Built the first version of the website.

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    $('#coverScreen').fadeIn(2000).delay(1000);
+    $('#coverScreen').fadeOut(1000);
+    $('nav, #buffer, #home').delay(4000).fadeIn(1000);
+});
+
+function swapScreens(title) {
+    var showing = $('article[showing]');
+    var selected = $("#" + title);
+
+    if (showing[0].id === title)
+        return;
+
+    if (selected[0].id === "gameplay") {
+        initSlider1();
+    }
+
+    showing.removeAttr("showing").fadeOut();
+    selected.attr("showing", "").delay(500).fadeIn();
+
+    selected = 0;
+}
